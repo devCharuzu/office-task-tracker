b95ef7bf7b8eac88018b41c38cd5564acde718fe
devCharuzu <jcpanganiban1@gmail.com>
2026-03-05 11:58:06 +0800

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,7 +1,10 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwLGja8-IDIYIBEENNZNw2FhGQnKEt3vGaRjX1ndB34LFVfnkNkcebUg9HaI0k4Di4/exec";
 
-// Global Data Store (Faster than refetching)
-let AppData = { tasks: [], users: [], session: JSON.parse(localStorage.getItem('office_session')) };
+let AppData = { 
+    tasks: [], 
+    users: [], 
+    session: JSON.parse(localStorage.getItem('office_session')) 
+};
 
 async function syncData() {
     try {
@@ -12,6 +15,7 @@ async function syncData() {
         return data;
     } catch (e) {
         console.error("Sync Failed", e);
+        return null;
     }
 }
 
